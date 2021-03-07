a6c1a7f2d6118989f763d285d39c933af6f49218
Maksym Ushakov <ushakov.m@hotmail.com>
2021-03-07 11:38:17 +0200

    feat: add brackets check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const bracketsConfigFlat = flattened(bracketsConfig).join("");
+    return !str.split("").reduce((acc, el) => {
+        let bracketIndex = bracketsConfigFlat.indexOf(el);
+        let isOpenBracket =
+            bracketIndex === bracketsConfigFlat.lastIndexOf(el)
+                ? !(bracketIndex % 2)
+                : !acc.length || acc[acc.length - 1].closeBarcket !== el;
+        if (isOpenBracket || !acc.length) {
+            acc.push({
+                el,
+                closeBarcket: bracketsConfigFlat[bracketIndex + 1] || "",
+            });
+        } else if (acc[acc.length - 1].closeBarcket === el) {
+            acc.pop();
+        }
+        return acc;
+    }, []).length;
+};
+
+const flattened = (arr) => [].concat(...arr);
